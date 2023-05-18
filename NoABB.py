9463607e9547defbb8056c81753ff2892e3cf94c
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-05-18 20:51:31 -0300

    Update NoABB.py

--- NoABB.py
+++ NoABB.py
@@ -1,18 +1,18 @@
 from Elemento import Elemento
 class No:
-    def __init__(self, elemento = Elemento()):
-        self.__elemento = elemento
+    def __init__(self):
+        self.__dados = Elemento() # Alterei aqui
         self.__filhoEsquerda = None
         self.__filhoDireita = None
     def getFilhoEsquerda(self):
         return self.__filhoEsquerda
     def setFilhoEsquerda(self, n):
         self.__filhoEsquerda = n
-    def getFilhoDireta(self):
+    def getFilhoDireita(self):
         return self.__filhoDireita
     def setFilhoDireita(self, n):
         self.__filhoDireita = n
-    def getElemento(self):
-        return self.__elemento
-    def setElemento(self, e):
-        self.__elemento = e
+    def getDados(self): # Mudei o nome do Método
+        return self.__dados
+    def setDados(self, d): # Mudei o nome do Método
+        self.__dados = d
