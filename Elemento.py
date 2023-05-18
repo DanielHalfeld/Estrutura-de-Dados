7eb3199ae48707268a4da58a538bee6cb15b19c9
DanielHalfeld <104243301+DanielHalfeld@users.noreply.github.com>
2023-05-18 20:51:11 -0300

    Update Elemento.py

--- Elemento.py
+++ Elemento.py
@@ -1,9 +1,19 @@
 class Elemento:
-  def __init__(self, v = 0):
-    self.__valor = v
-  def getValor(self):
-    return self.__chave 
-  def setValor(self, v):
-    self.__valor = v 
-  def getElemento(self):
-    return self.getChave(), self.getNome()
+	def __init__(self):
+		self.__chave = 0
+		self.__nome = ""
+
+	def getChave(self):
+		return self.__chave
+
+	def setChave(self, valor):
+		self.__chave = valor
+
+	def getNome(self):
+		return self.__nome
+
+	def setNome(self, n):
+		self.__nome = n
+
+	def getValores(self):
+		return self.__chave, self.__nome
